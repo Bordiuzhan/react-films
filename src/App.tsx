e483fa308cd5b1aadedf5a28651a336fa47f5f3e
Bordiuzhan <bordyujan@gmail.com>
2024-05-08 16:15:17 +0200

    new one

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,8 @@ function App() {
             </header>
             <main>
                 <Outlet/>
-                <div className={styles.container}>
-                    <h1>App</h1>
-                </div>
+
+                kjhbfikewbvihwb
             </main>
             <footer>
                 <Footer/>
