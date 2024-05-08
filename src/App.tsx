b706036f7a9e9647c9eca03dfbc01524cbe603a6
Bordiuzhan <bordyujan@gmail.com>
2024-05-08 15:46:48 +0200

    test 2

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,6 @@ function App() {
             </header>
             <main>
                 <Outlet/>
-                <h1> HELLO 1!!</h1>
-                <h1> HELLO !1!</h1>
                 <h1> HELLO !!1</h1>
             </main>
             <footer>
