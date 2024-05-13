6750f475266688e21d701a040557e3303340bab9
Bordiuzhan <bordyujan@gmail.com>
2024-05-13 10:59:09 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,6 @@ function App() {
             </header>
             <main>
                 <Outlet/>
-                <div className={styles.container}>
-                    <h1>App</h1>
-                </div>
-                <div className={styles.container}>
-                    <h1>App</h1>
-                </div>
             </main>
             <footer>
                 <Footer/>
